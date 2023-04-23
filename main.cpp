19d7e139374d528588b4ea19aa124d1fa1f6308d
5059968 <128074987+5059968@users.noreply.github.com>
2023-04-23 14:05:11 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int t1 = 1, t2 = 1, t3=0, n;
+
+    cin >> n;
+    cout<<t1 <<endl;
+    cout<< t2 <<endl;
+
+    t3 = t1 + t2;
+    if (n>=2) {
+        while (t3 <= n) {
+            cout << t3 << endl;
+            t1 = t2;
+            t2 = t3;
+            t3 = t1 + t2;
+        }
+    } else{
+        cout<<"errore"<<endl;
+    }
+    return 0;
 }
+
